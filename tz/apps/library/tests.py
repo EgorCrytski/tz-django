50d6b08c568064c4cd09bec399b953b013f36bcb
Egor Crytski <egorcrytski@gmail.com>
2020-05-07 00:38:36 +0300

    added tests for unauthorized, unauthenticated users

--- tz/apps/library/tests.py
+++ tz/apps/library/tests.py
@@ -59,6 +59,26 @@ class TestLibraryUserAdd(APITestCase):
                          'Expected Response Code 400, received {0} instead.'
                          .format(response.status_code))
 
+    def test_unauthorized_add_user(self):
+        user = User
+        request = self.factory.post(self.url, {"username": "TestUser"}, format='json')
+        # force_authenticate(request, user=user, token=user.auth_token)
+        response = self.view(request)
+        print('\n' + request.build_absolute_uri())
+        self.assertEqual(response.status_code, 401,
+                         'Expected Response Code 401, received {0} instead.'
+                         .format(response.status_code))
+
+    def test_forbidden_add_user(self):
+
+        request = self.factory.post(self.url, {"username": "TestUser"}, format='json')
+        force_authenticate(request, user=self.user, token=self.user)
+        response = self.view(request)
+        print('\n' + request.build_absolute_uri())
+        self.assertEqual(response.status_code, 403,
+                         'Expected Response Code 403, received {0} instead.'
+                         .format(response.status_code))
+
 
 class TestLibraryUserDetail(APITestCase):
     def setUp(self):
@@ -75,6 +95,28 @@ class TestLibraryUserDetail(APITestCase):
         response = self.view(request, uid=self.user.id)
         self.assertEqual(response.status_code, 200,
                          'Expected Response Code 200, received {0} instead.'
+                         .format(response.status_code))
+
+    def test_unauthorised_user_detail(self):
+
+        self.url = reverse('library:api_user_detail', kwargs={'uid': self.user.id})
+        request = self.factory.get(self.url, uid=self.user.id)
+        #force_authenticate(request, user=user, token=self.user.auth_token)
+        print('\n' + request.build_absolute_uri())
+        response = self.view(request, uid=self.user.id)
+        self.assertEqual(response.status_code, 401,
+                         'Expected Response Code 401, received {0} instead.'
+                         .format(response.status_code))
+
+    def test_forbidden_user_detail(self):
+        user = User
+        self.url = reverse('library:api_user_detail', kwargs={'uid': self.user.id})
+        request = self.factory.get(self.url, uid=self.user.id)
+        force_authenticate(request, user=self.user, token=self.user)
+        print('\n' + request.build_absolute_uri())
+        response = self.view(request, uid=self.user.id)
+        self.assertEqual(response.status_code, 403,
+                         'Expected Response Code 403, received {0} instead.'
                          .format(response.status_code))
 
 
@@ -135,7 +177,7 @@ class TestLibraryBookAdd(APITestCase):
     def setUp(self):
         self.user = User.objects.create(username='John')
         self.factory = APIRequestFactory()
-        self.url = reverse('library:api_book_add',)
+        self.url = reverse('library:api_book_add', )
         self.view = views.BookCreateView.as_view()
 
     def test_get_add_book(self):
@@ -188,10 +230,10 @@ class TestLibraryBookDetail(APITestCase):
     def test_book_detail(self):
         user = User
         self.url = reverse('library:api_book_detail', kwargs={'uid': self.user.id, 'bid': 1})
-        request = self.factory.get(self.url, uid=self.user.id, bid = 1)
+        request = self.factory.get(self.url, uid=self.user.id, bid=1)
         force_authenticate(request, user=user, token=user)
         print('\n' + request.build_absolute_uri())
-        response = self.view(request, uid=self.user.id, bid = 1)
+        response = self.view(request, uid=self.user.id, bid=1)
         self.assertEqual(response.status_code, 200,
                          'Expected Response Code 200, received {0} instead.'
                          .format(response.status_code))
@@ -213,10 +255,10 @@ class TestLibraryBookEdit(APITestCase):
         request = self.factory.put(self.url, data={"book_name": "book1",
                                                    "book_author": "author1",
                                                    "book_year": "1212-12-21T00:00:00Z", }
-                                   , bid=1, uid =self.user.id )
+                                   , bid=1, uid=self.user.id)
         print('\n' + request.build_absolute_uri())
         force_authenticate(request, user=user, token=user)
-        response = self.view(request, bid=1, uid = self.user.id)
+        response = self.view(request, bid=1, uid=self.user.id)
         self.assertEqual(response.status_code, 200,
                          'Expected Response Code 200, received {0} instead.'
                          .format(response.status_code))
@@ -238,7 +280,7 @@ class TestLibraryBookDelete(APITestCase):
         request = self.factory.post(self.url, uid=self.user.id, bid=1)
         force_authenticate(request, user=user, token=user)
         print('\n' + request.build_absolute_uri())
-        response = self.view(request, uid = self.user.id, bid=1)
+        response = self.view(request, uid=self.user.id, bid=1)
         self.assertEqual(response.status_code, 200,
                          'Expected Response Code 200, received {0} instead.'
                          .format(response.status_code))
