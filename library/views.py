d8870bb33893ea394dbd7925d5706732e34a9167
EgorCrytski <32506081+EgorCrytski@users.noreply.github.com>
2020-05-13 10:32:19 +0300

    Update views.py

--- library/views.py
+++ library/views.py
@@ -2,7 +2,6 @@ from django.http import Http404, HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import render, get_object_or_404
 from rest_framework import permissions
-# from .permissions import IsOwner
 from .models import Book, User
 from django.contrib.auth.models import User
 from rest_framework.views import APIView
