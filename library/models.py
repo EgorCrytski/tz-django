15180d4789051e581476aba060552cee57f10769
EgorCrytski <32506081+EgorCrytski@users.noreply.github.com>
2020-05-13 10:01:36 +0300

    Update models.py

--- library/models.py
+++ library/models.py
@@ -2,20 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-
-'''
-class User(models.Model):
-    user_name = models.CharField('User name', max_length=100)
-
-    def __str__(self):
-        return self.user_name
-
-    class Meta:
-        verbose_name = 'User'
-        verbose_name_plural = 'Users'
-'''
-
-
 class Book(models.Model):
     book_name = models.CharField('Book name', max_length=100)
     book_author = models.CharField('Book author', max_length=100)
