3229519632de32cfe543612c9f866299d45075ea
Egor Crytski <egorcrytski@gmail.com>
2020-05-20 15:10:49 +0300

    testing changes

--- library/tests/test_user.py
+++ library/tests/test_user.py
@@ -5,26 +5,26 @@ from .. import views
 
 
 class Helper(APITestCase):
-    def get_helper(self, force, code, kwargs = False, user = User):
+    def get_helper(self, force, code, kwargs=False, user=User):
         if kwargs == False:
             request = self.factory.get(self.url)
         else:
-            request = self.factory.get(self.url, uid = user.id)
+            request = self.factory.get(self.url, uid=user.id)
         if force == True:
-            force_authenticate(request, user= user)
-        #print(request.build_absolute_uri())
+            force_authenticate(request, user=user)
+        # print(request.build_absolute_uri())
         if kwargs == False:
             response = self.view(request)
         else:
-            response = self.view(request, uid = user.id)
+            response = self.view(request, uid=user.id)
         self.assertEqual(response.status_code, code,
                          'Expected Response Code {0}, received {1} instead.'
                          .format(code, response.status_code))
 
-    def post_helper(self, data, force, code, user = User):
+    def post_helper(self, data, force, code, user=User):
         request = self.factory.post(self.url, data, format='json')
         if force == True:
-            force_authenticate(request, user= user)
+            force_authenticate(request, user=user)
         response = self.view(request)
 
         self.assertEqual(response.status_code, code,
@@ -39,16 +39,12 @@ class TestLibraryUserList(APITestCase):
         self.view = views.UserListView.as_view()
         self.user = User.objects.create(username='John')
 
-
-
     def test_GetUserList(self):
         Helper.get_helper(self, force=True, code=200)
 
 
-
 class TestLibraryUserAdd(APITestCase):
     def setUp(self):
-
         self.factory = APIRequestFactory()
         self.url = reverse('library:api_user_add')
         self.view = views.UserCreateView.as_view()
@@ -57,43 +53,35 @@ class TestLibraryUserAdd(APITestCase):
     def test_get_add_user(self):
         Helper.get_helper(self, force=True, code=405)
 
-
     def test_add_user(self):
         Helper.post_helper(self, data={"username": "TestUser"}, force=True, code=201)
-
 
     def test_add_empty_user(self):
         Helper.post_helper(self, data={"username": ""}, force=True, code=400)
 
-
     def test_unauthorized_add_user(self):
         Helper.post_helper(self, data={"username": "TestUser"}, force=False, code=401)
 
-
     def test_forbidden_add_user(self):
-        Helper.post_helper(self, data={"username": "TestUser"}, force=True, code=403, user = self.user)
-
+        Helper.post_helper(self, data={"username": "TestUser"}, force=True, code=403, user=self.user)
 
 
 class TestLibraryUserDetail(APITestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='John')
-        self.superuser = User.objects.create_superuser(username = "Admin")
+        self.superuser = User.objects.create_superuser(username="Admin")
         self.factory = APIRequestFactory()
         self.view = views.UserDetailView.as_view()
         self.url = reverse('library:api_user_detail', kwargs={'uid': self.user.id})
 
     def test_user_detail(self):
-        Helper.get_helper(self, force = True, code = 200, kwargs=True, user = self.superuser)
-
+        Helper.get_helper(self, force=True, code=200, kwargs=True, user=self.superuser)
 
     def test_unauthorised_user_detail(self):
         Helper.get_helper(self, force=False, code=401, kwargs=True, user=self.user)
 
-
     def test_forbidden_user_detail(self):
-        Helper.get_helper(self, force=True, code = 403, kwargs=True, user = self.user)
-
+        Helper.get_helper(self, force=True, code=403, kwargs=True, user=self.user)
 
 
 class TestLibraryUserEdit(APITestCase):
@@ -101,7 +89,6 @@ class TestLibraryUserEdit(APITestCase):
         self.user = User.objects.create(username='John')
         self.factory = APIRequestFactory()
         self.view = views.UserEditView.as_view()
-
 
     def test_user_edit(self):
         user = User
