d6ae907db0a61adf92f200dd9dcfefaf4e5e763f
Egor Crytski <egorcrytski@gmail.com>
2020-05-29 14:17:53 +0300

    added django-cors-headers

--- tz/settings/base.py
+++ tz/settings/base.py
@@ -44,9 +44,11 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'library.apps.LibraryConfig',
     'drf_yasg',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -173,3 +175,5 @@ SWAGGER_SETTINGS = {
     },
     'USE_SESSION_AUTH': False,
 }
+
+CORS_ORIGIN_ALLOW_ALL = True
